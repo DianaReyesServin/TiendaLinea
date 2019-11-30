e7090700c06925f8d080a13914b83b638a9e83a8
Miguel Angel Hernandez Venegas <miguel.angel.herven@gmail.com>
2019-11-29 19:44:36 -0600

    Creación de componentes Home, ProductDetail y el servicio Product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductdetailComponent } from './pages/productdetail/productdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    ProductdetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/productdetail/productdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productdetail',
+  templateUrl: './productdetail.component.html',
+  styles: []
+})
+export class ProductdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
