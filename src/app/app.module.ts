cfbd347e20dcf9f894c0e442d49bb54a7e3a8bc2
andromedagalaxia <55502148+andromedagalaxia@users.noreply.github.com>
2019-11-29 20:22:14 -0600

    crear componente navbar, cards y products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './pages/about/about.component';
-<<<<<<< HEAD
+
 import { HomeComponent } from './pages/home/home.component';
 import { ProductdetailComponent } from './pages/productdetail/productdetail.component';
-=======
+import { ProductsComponent } from './pages/products/products.component';
+import { CardsComponent } from './components/cards/cards.component';
+
 import { AppRoutingModule } from './app.routes';
->>>>>>> 219c99138f8ec53a7a0c0fd2e49b4021254b4fb7
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     HomeComponent,
-    ProductdetailComponent
+    ProductdetailComponent,
+    ProductsComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
