032ce6dfec2d65c1f6e01d8df26b9c3a6df3bfbc
Miguel Angel Hernandez Venegas <miguel.angel.herven@gmail.com>
2019-12-03 15:09:59 -0600

    Se agrega el productService, imagenes para el funconamiento de componentes, y se le agrega diseno a HomeComponent

---
+++ src/app/models/models.ts
@@ -0,0 +1,7 @@
+export interface Producto{codigo?: string,
+    descripcion?: string,
+    categoria?: string, 
+    proveedor?:string,
+    provDescr?:string,
+    precio?: number
+   }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../models/models';
+import { Observable } from 'rxjs';
+
+const URL_PRODUCTS='https://angular-products-mahv.firebaseio.com/productos.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+
+  }
+
+  getAll(){
+    return this.http.get(URL_PRODUCTS);
+  }
+
+  getCategory(category:string){
+    return new Observable(observer=>{
+      this.http.get(URL_PRODUCTS).subscribe((data:Producto[])=>{
+        const filter = data.filter(item=>item.categoria==category || item.categoria.indexOf(category)>0);
+        observer.next(filter);
+      })
+    });
+  }
+
+  getByCode(code:string){
+    return new Observable(observer=>{
+      this.getAll().subscribe((data:Producto[])=>{
+        const filter = data.filter(item=>item.codigo==code);
+        observer.next(filter[0]);
+      })
+    });
+  }
 }
