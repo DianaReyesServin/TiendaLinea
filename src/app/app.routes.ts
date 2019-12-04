7f0408728011f0e999f4f938dc60eb2edf5b0297
Diana Reyes <dia_nolis@hotmail.com>
2019-12-04 17:53:20 -0600

    se modificaron las rutas para los productos por categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,18 @@ import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { PagesnotfoundComponent } from './pages/pagesnotfound/pagesnotfound.component';
+import { CategoriesComponent } from './pages/products/categories/categories.component';
 
 const routes: Routes = [
     { path: 'about', component: AboutComponent, data: {title: 'Acerca de', icon: 'fa-prescription-bottle'}},
     { path: 'home', component: HomeComponent, data: {title: 'Inicio', icon: 'fa-home-lg'} },
-    { path: 'products', component: ProductsComponent, data: {title: 'Productos', icon: 'fa-bags-shopping'} },
+    { path: 'products', component: ProductsComponent, data: {title: 'Productos', icon: 'fa-bags-shopping'},
+    children: [
+        { path: 'categories/:category', component: CategoriesComponent },
+        { path: '', redirectTo: 'categories/Cards', pathMatch: 'full'},
+        { path: '**', redirectTo: 'categories/Cards', pathMatch: 'full'}
+    ]
+},
     { path: '', component: HomeComponent },
     { path: '**', component: PagesnotfoundComponent }
 ];
