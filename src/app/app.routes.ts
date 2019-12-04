ba905c1d704603f844e66764281c34192d04c657
Diana Reyes <dia_nolis@hotmail.com>
2019-12-03 22:06:19 -0600

    se modificaron las rutas para el breadcrumb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,23 +6,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { PagesnotfoundComponent } from './pages/pagesnotfound/pagesnotfound.component';
 
-// import { Name2Component } from './';
-// import { Name3Component } from './';
-// import { Name4Component } from './';
-// import { PageNotFoundComponent } from './';
-
 const routes: Routes = [
-    { path: 'about', component: AboutComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'products', component: ProductsComponent },
+    { path: 'about', component: AboutComponent, data: {title: 'Acerca de', icon: 'fa-prescription-bottle'}},
+    { path: 'home', component: HomeComponent, data: {title: 'Inicio', icon: 'fa-home-lg'} },
+    { path: 'products', component: ProductsComponent, data: {title: 'Productos', icon: 'fa-bags-shopping'} },
     { path: '', component: HomeComponent },
     { path: '**', component: PagesnotfoundComponent }
-
-    //{ path: 'path/:routeParam', component: MyComponent },
-    //{ path: 'staticPath', component: ... },
-    //{ path: '**', component: ... },
-    //{ path: 'oldPath', redirectTo: '/staticPath' },
-    //{ path: ..., component: ..., data: { message: 'Custom' }
 ];
 
 @NgModule({
