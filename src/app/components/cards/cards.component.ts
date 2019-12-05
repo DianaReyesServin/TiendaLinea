c008f291004f88bd114afd41afae4d68295f89da
Ma Guadalupe Brizuela Gamiño <brizuelautl@gmail.com>
2019-12-05 09:52:37 -0600

    componente card funcionando con boton para ir a detalle de productos

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Producto } from 'src/app/models/models';
+
 
 @Component({
   selector: 'app-cards',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CardsComponent implements OnInit {
+@Input () products:Producto[];
+category: string = '';
 
   constructor() { }
 
   ngOnInit() {
   }
+  colorLabel(precio: number){
+    let color: string='';
+   
+    if (precio > 0 &&  precio <=30000)
+    color ='warning';
+    if (precio > 30000 &&  precio <=50000)
+    color ='success';  
+    if (precio > 50000)
+    color = 'danger';
+  
+    return color;
+  }
+
 
 }
