cecc56cb8ab78357544e9d1767bfe4f9795a36c6
Diana Reyes <dia_nolis@hotmail.com>
2019-12-05 21:09:56 -0600

    se agrego la ruta de detalle de productos y se modificaron los iconos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,18 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { PagesnotfoundComponent } from './pages/pagesnotfound/pagesnotfound.component';
 import { CategoriesComponent } from './pages/products/categories/categories.component';
+import { ProductdetailComponent } from './pages/productdetail/productdetail.component';
 
 const routes: Routes = [
-    { path: 'about', component: AboutComponent, data: {title: 'Acerca de', icon: 'fa-prescription-bottle'}},
-    { path: 'home', component: HomeComponent, data: {title: 'Inicio', icon: 'fa-home-lg'} },
-    { path: 'products', component: ProductsComponent, data: {title: 'Productos', icon: 'fa-bags-shopping'},
+    { path: 'about', component: AboutComponent, data: {title: 'Acerca de', icon: 'fa-address-card'}},
+    { path: 'home', component: HomeComponent, data: {title: 'Inicio', icon: 'fa-home'} },
+    { path: 'products', component: ProductsComponent, data: {title: 'Productos', icon: 'fa-car'},
     children: [
         { path: 'categories/:category', component: CategoriesComponent },
         { path: '', redirectTo: 'categories/Cards', pathMatch: 'full'},
         { path: '**', redirectTo: 'categories/Cards', pathMatch: 'full'}
-    ]
-},
+    ]},
+    { path: 'product/:code/:category', component: ProductdetailComponent },
     { path: '', component: HomeComponent },
     { path: '**', component: PagesnotfoundComponent }
 ];
