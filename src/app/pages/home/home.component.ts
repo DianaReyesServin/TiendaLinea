8af0ed2889963397474560c15dc8416a0ccd984d
Diana Reyes <dia_nolis@hotmail.com>
2019-12-12 23:26:21 -0600

    Se agrego el pnotify con disenio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import PNotify from 'pnotify/dist/es/PNotify';
-import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
+import PNotify from 'pnotify/dist/es/PNotify';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
 
 
 @Component({
@@ -10,21 +10,22 @@ import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { 
-        PNotifyButtons; // Initiate the module. Important!
-        PNotify.alert({
-          title: "Bienvenido!!",
-          text:"Proyecto de Miguel, Diana y Lupita",
-          animation: 'fade',
-          icon: 'icon-bell-check',
-          addClass: 'alert alert-success border-success alert-styled-right alert-arrow-right',
-          shadow:true,
-          hide:true,
-          delay:1000*10,
-          type:'success'
-        });
-      }
-    
+  constructor() { 
+    PNotifyButtons; // Initiate the module. Important!
+    PNotify.alert({
+      title: "Bienvenido",
+      text:"Tarea de Miguel, Diana y Lupita",
+      animation: 'fade',
+      icon: 'icon-bell-check',
+      addClass: 'alert alert-success border-success alert-styled-right alert-arrow-right',
+      shadow:true,
+      hide:true,
+      delay:1000*10,
+      type:'success'
+    });
+  }
+
   ngOnInit() {
   }
+
 }
